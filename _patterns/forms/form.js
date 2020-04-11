6c8ce6a5da1d72faca6fd6bbd3aacd9db9128f85
Jacob Bradley <brad0351@algonquinlive.com>
2020-04-11 14:39:17 -0400

    Update config file
    Completed config.yml for buttons. Submit for marks.

--- _patterns/forms/form.js
+++
@@ -1,9 +0,0 @@
-$('#same-info').change(function () {
-  if ($('#same-info').is(':checked')) {
-    $('.shipping-info input, .shipping-info select, .shipping-info textarea').attr('disabled', true);
-    $('.shipping-info').addClass('shipping-disabled');
-  } else {
-    $('.shipping-info input, .shipping-info select, .shipping-info textarea').attr('disabled', false);
-    $('.shipping-info').removeClass('shipping-disabled');
-  }
-});
